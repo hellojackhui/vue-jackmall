966203a803f8edcf08f63ef5cb1a3ff0f70ebe55
hellojackhui <HUIJIAWEI@hotmail.com>
2017-10-08 18:38:57 +0800

    全局模态框组件实现

--- src/views/GoodsList.vue
+++ src/views/GoodsList.vue
@@ -9,8 +9,12 @@
         <div class="filter-nav">
           <span class="sortby">Sort by:</span>
           <a href="javascript:void(0)" class="default cur">Default</a>
-          <a href="javascript:void(0)" @click="sortGoods" class="price">Price
-            <svg class="icon icon-arrow-short"><use xlink:href="#icon-arrow-short"></use></svg></a>
+          <a href="javascript:void(0)" @click="sortGoods" >Price
+            <svg class="icon-arrow-short" v-bind:class="{'sort-up':!sortFlag}">
+              <use xlink:href="#icon-arrow-short" xmlns:xlink="http://www.w3.org/1999/xlink">
+              </use>
+            </svg>
+          </a>
           <a href="javascript:void(0)" class="filterby stopPop" @click="showFilterPop">Filter by</a>
         </div>
         <div class="accessory-result">
@@ -55,15 +59,44 @@
       </div>
     </div>
     <div class="md-overlay" v-show="overLayFlag" @click="closePop"></div>
+    <modal v-bind:mdShow="mdShow" v-on:close="closeModal">
+        <b slot="message">
+          请先登录，否则无法加入到购物车!
+        </b>
+        <div slot="btnGroup">
+          <a  class="btn btn--m" @click="mdShow = false" href="javascript:;">关闭</a>
+        </div>
+    </modal>
+
+    <modal v-bind:mdShow="mdShowCart" v-on:close="closeModal">
+      <b slot="message">
+        <svg class="icon-status-ok">
+          <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-status-ok"></use>
+        </svg>
+        <span>加入购物车成功！</span>
+      </b>
+      <div slot="btnGroup">
+        <a  class="btn btn--m" @click="mdShowCart = false" href="javascript:;">继续购物</a>
+        <router-link class="btn btn--m" href="javascript:;" to="/cart">查看购物车</router-link>
+      </div>
+    </modal>
+
     <nav-footer></nav-footer>
    </div>
 </template>
+<style>
+  .sort-up{
+    transform: rotate(180deg);
+    transition: all .5s ease-out;
+  }
+</style>
 <script>
   import './../assets/css/base.css'
   import './../assets/css/product.css'
   import NavHeader from '../components/NavHeader.vue'
   import NavFooter from '../components/NavFooter.vue'
   import NavBread from '../components/NavBread.vue'
+  import Modal from './../components/Modal.vue'
   import axios from 'axios'
 
   export default{
@@ -95,13 +128,16 @@
         page:1,
         pageSize:8,
         busy:true,
-        loading:false
+        loading:false,
+        mdShow:false,
+        mdShowCart:false
       }
     },
     components:{
         NavHeader:NavHeader,
         NavFooter:NavFooter,
-        NavBread:NavBread
+        NavBread:NavBread,
+        Modal:Modal
     },
     mounted() {
       this.getGoodsList();
@@ -169,15 +205,17 @@
               }).then((res)=>{
               var res = res.data;
                  if(res.status == 0){
-                   alert('加入成功');
-            //      this.mdShowCart = true;
+                   this.mdShowCart = true;
             //      this.$store.commit("updateCartCount",1);
                  }else{
-                   alert("msg:"+res.msg);
-            //        this.mdShow = true;
+                    this.mdShow = true;
                  }
           });
       },
+      closeModal(){
+          this.mdShow = false;
+
+      }
     }
   }
 </script>
